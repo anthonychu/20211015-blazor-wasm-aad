4090fd293af29dee0b53a11130e81c5a3c77f300
Anthony Chu <anthony@anthonychu.ca>
2021-10-15 23:25:37 -0700

    Initial commit

---
+++ BlazorWasmAad/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorWasmAad;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes
+        .Add("https://graph.microsoft.com/User.Read");
+    options.ProviderOptions.LoginMode = "redirect";
+});
+
+await builder.Build().RunAsync();
